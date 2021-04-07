e1a395e8604aaa1f51d2d997f953f4646280e747
P-E-TT-E-R <37306965+P-E-TT-E-R@users.noreply.github.com>
2021-04-07 20:55:17 +0200

    Basics
    Data from broken repo.

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, session, jsonify, request
+from datetime import datetime
+import uuid, json
+
+app = Flask(__name__)
+app.secret_key = '7fbb7490f9d2571fc3ef5ac265220d167812157ff520ba29ca47f3a5c8e0332d'
+
+@app.route('/')
+def index():
+   for x in range(0,5):
+      key = uuid.uuid4().hex
+      if key not in orders:
+         orders[key] = {
+            "created": str(datetime.now()),
+            "users": [],
+            "restaurants": [],
+            "stage": 1,
+            "votes": {
+               "stage2": [],
+               "stage3": [],
+               "stage4": []
+            }
+         }
+         session['key'] = key
+         hostname = "127.0.0.1:8000"
+         joinUrl = f"{hostname}/api/join/{key}"
+         return render_template("main/index.html",key=key,joinUrl=joinUrl)
+
+@app.route('/order')
+def order():
+   return render_template("main/order.html", key=session['key'])
+
+@app.route('/api/info/<id>')
+def apiInfo(id):
+   return jsonify(orders[id])
+
+@app.route('/api/join/<id>')
+def apiJoin(id,name = ''):
+   return render_template("main/join.html", key=id)
+
+@app.route('/api/action/<id>',methods=['GET'])
+def apiSubmit(id):
+   type = request.args.get('type')
+   if type == "join":
+      name = request.args.get('name')
+      session['name'] = name
+      orders[id]['users'].append(name)
+
+   if type == "start":
+      orders[id]['stage'] = 2
+
+   if type == "vote":
+      stage = request.args.get('stage')
+      orders[id]['votes'][stage].append((session['name'], request.args.get('value')))
+
+   return "Delivered Successfully"
+
+if __name__ == '__main__':
+   orders = {}
+   app.run('0.0.0.0',8000)
---
+++ static/assets/js/main.js
@@ -0,0 +1,19 @@
+function join(key) {
+    console.log("Joining");
+	var nick = document.getElementById("username").value;
+	console.log(httpRequest('api/action/'+key+'?type=join&name='+nick));
+	window.location.replace("order");
+}
+function begin(key) {
+    console.log("Beginning");
+    console.log(httpRequest('api/action/'+key+'?type=join&name=Host'));
+	console.log(httpRequest('api/action/'+key+'?type=start'));
+	window.location.replace("order");
+}
+async function httpRequest(loc) {
+    console.log("HTTP Request > http://"+window.location.host + '/' + loc);
+    url = 'http://' + window.location.host + '/' + loc
+	let request = fetch(url)
+    let response = request.text
+    return response
+}
