16c2f18613fe13483b1b36cb82be629bdaf6bc6f
P-E-TT-E-R <37306965+P-E-TT-E-R@users.noreply.github.com>
2021-04-08 17:52:21 +0200

    JS, CSS, HTML changes

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,26 +1,29 @@
 var info;
 
-setInterval(getInfo, 1500);
+if (typeof key !== 'undefined') {
+    setInterval(getInfo, 1500);
+    setInterval(updateUserlist, 1500);
+}
 
-function join() {
+async function join() {
     console.log("Joining");
 	var nick = document.getElementById("username").value;
-	console.log(httpRequest('api/action/'+key+'?type=join&name='+nick));
-	window.location.replace("order");
+	console.log(await httpRequest('api/action/'+key+'?type=join&name='+nick));
+	window.location.replace("/order");
 }
-function begin() {
+async function begin() {
     console.log("Beginning");
-    console.log(httpRequest('api/action/'+key+'?type=join&name=Host'));
-	console.log(httpRequest('api/action/'+key+'?type=start'));
-	window.location.replace("order");
+    console.log(await httpRequest('api/action/'+key+'?type=join&name=Host'));
+	console.log(await httpRequest('api/action/'+key+'?type=start'));
+	window.location.replace("/order");
 }
 function updateUserlist() {
     var html = ""
     info['users'].forEach(function(user) {
         html += `<h3><i class="bi bi-person-circle"></i>${user}</h3>`
     })
-    getElementById('list-users').innerHTML = ''
-    getElementById('list-users').innerHTML = html
+    document.getElementById('list-users').innerHTML = ''
+    document.getElementById('list-users').innerHTML = html
 }
 async function getInfo () {
     info = await httpRequest('api/info/'+key);
@@ -29,6 +32,10 @@ async function httpRequest(loc) {
     url = 'http://' + window.location.host + '/' + loc;
     console.log("HTTP Request > "+url);
     let response = await fetch(url);
-
-    return await response.json();
+    text = await response.text();
+    try {
+        return JSON.parse(text)
+    } catch {
+        return text
+    }
 }
