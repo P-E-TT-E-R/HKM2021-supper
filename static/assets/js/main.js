95e10cd9beba35b1b2be8d962bbbd64f19321250
P-E-TT-E-R <37306965+P-E-TT-E-R@users.noreply.github.com>
2021-04-08 11:54:46 +0200

    JS update (not in working state yet)

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,25 +1,34 @@
-function join(key) {
+var info;
+
+setInterval(getInfo, 1500);
+
+function join() {
     console.log("Joining");
 	var nick = document.getElementById("username").value;
 	console.log(httpRequest('api/action/'+key+'?type=join&name='+nick));
 	window.location.replace("order");
 }
-function begin(key) {
+function begin() {
     console.log("Beginning");
     console.log(httpRequest('api/action/'+key+'?type=join&name=Host'));
 	console.log(httpRequest('api/action/'+key+'?type=start'));
 	window.location.replace("order");
 }
-function updateUserlist(key) {
-    getElementById
+function updateUserlist() {
+    var html = ""
+    info['users'].forEach(function(user) {
+        html += `<h3><i class="bi bi-person-circle"></i>${user}</h3>`
+    })
+    getElementById('list-users').innerHTML = ''
+    getElementById('list-users').innerHTML = html
 }
-function getInfo (key) {
-    return JSON.parse(httpRequest('api/info/'+key));
+async function getInfo () {
+    info = await httpRequest('api/info/'+key);
 }
 async function httpRequest(loc) {
-    console.log("HTTP Request > http://"+window.location.host + '/' + loc);
-    url = 'http://' + window.location.host + '/' + loc
-	let request = fetch(url)
-    let response = request.text
-    return response
+    url = 'http://' + window.location.host + '/' + loc;
+    console.log("HTTP Request > "+url);
+    let response = await fetch(url);
+
+    return await response.json();
 }
